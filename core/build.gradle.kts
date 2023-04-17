b533445cf290fbaa0ac1dd7978746db6171d8169
Brian Giori <brian.giori@amplitude.com>
2023-04-17 15:11:27 -0700

    0.0.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -51,7 +51,7 @@ publishing {
     publications.withType<MavenPublication> {
         groupId = "com.amplitude"
         artifactId = "evaluation-proxy-core"
-        version = "0.1.0"
+        version = "0.0.1"
         artifact(javadocJar)
         pom {
             name.set("Amplitude Evaluation Proxy")
