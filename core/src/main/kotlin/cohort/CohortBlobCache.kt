aa9f683c58f4702f59ea39b92bb294cab5bd0bc9
Vaibhav Jain <vaibhav.jain@amplitude.com>
2025-12-05 21:26:58 +0530

    fix: lint

--- core/src/main/kotlin/cohort/CohortBlobCache.kt
+++ core/src/main/kotlin/cohort/CohortBlobCache.kt
@@ -15,29 +15,39 @@ internal class CohortBlobCache {
     private val map = HashMap<String, CacheEntry>()
     private val cachedVersions = HashMap<String, Long>()
 
-    private fun key(cohortId: String, lastModified: Long): String =
-        "$cohortId:$lastModified"
+    private fun key(
+        cohortId: String,
+        lastModified: Long,
+    ) = "$cohortId:$lastModified"
 
-    suspend fun get(cohortId: String, lastModified: Long): ByteArray? =
+    suspend fun get(
+        cohortId: String,
+        lastModified: Long,
+    ): ByteArray? =
         lock.withLock {
             map[key(cohortId, lastModified)]?.bytes
         }
 
-    suspend fun put(cohortId: String, lastModified: Long, bytes: ByteArray) =
-        lock.withLock {
-            // Evict old version if exists
-            cachedVersions[cohortId]?.let { oldLastModified ->
-                map.remove(key(cohortId, oldLastModified))
-            }
-            cachedVersions[cohortId] = lastModified
-            map[key(cohortId, lastModified)] = CacheEntry(bytes)
+    suspend fun put(
+        cohortId: String,
+        lastModified: Long,
+        bytes: ByteArray,
+    ) = lock.withLock {
+        // Evict old version if exists
+        cachedVersions[cohortId]?.let { oldLastModified ->
+            map.remove(key(cohortId, oldLastModified))
         }
+        cachedVersions[cohortId] = lastModified
+        map[key(cohortId, lastModified)] = CacheEntry(bytes)
+    }
 
-    suspend fun remove(cohortId: String, lastModified: Long) =
-        lock.withLock {
-            map.remove(key(cohortId, lastModified))
-            if (cachedVersions[cohortId] == lastModified) {
-                cachedVersions.remove(cohortId)
-            }
+    suspend fun remove(
+        cohortId: String,
+        lastModified: Long,
+    ) = lock.withLock {
+        map.remove(key(cohortId, lastModified))
+        if (cachedVersions[cohortId] == lastModified) {
+            cachedVersions.remove(cohortId)
         }
+    }
 }
